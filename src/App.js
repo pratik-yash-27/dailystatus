b4d90970558e7c28f9c62bb05b363a8fd574046f
pratikjagadale <pratik.jagadale@yash.com>
2022-09-28 17:44:51 +0530

    calculator complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Calculator from './Components/Calculator';
+
+function App() {
+  return (
+    <>
+      <h2 data-text="Calculator">Calculator</h2>
+      <div className="App">
+        <Calculator />
+      </div>
+    </>
+  );
+}
+
+export default App;
